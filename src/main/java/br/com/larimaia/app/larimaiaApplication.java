74724bda92922e181e1a5dea4db7c3298d3a2e16
Lucas <lucameiokilo@gmail.com>
2015-09-23 21:41:16 -0300

    add pacote rest e app

---
+++ src/main/java/br/com/larimaia/app/larimaiaApplication.java
@@ -0,0 +1,7 @@
+package br.com.larimaia.app;
+
+/**
+ * Created by Usuario on 23/09/2015.
+ */
+public class larimaiaApplication {
+}
---
+++ src/main/java/br/com/larimaia/rest/ProdutoResources.java
@@ -0,0 +1,7 @@
+package br.com.larimaia.rest;
+
+/**
+ * Created by Usuario on 23/09/2015.
+ */
+public class ProdutoResources {
+}
