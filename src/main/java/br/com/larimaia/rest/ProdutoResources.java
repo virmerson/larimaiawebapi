289c39cce7dfdfa6f29ab7d4705944c1c5066895
Lucas <lucameiokilo@gmail.com>
2015-09-23 22:13:02 -0300

    Atualização de ClienteResources e ProdutoResources

--- src/main/java/br/com/larimaia/rest/ProdutoResources.java
+++ src/main/java/br/com/larimaia/rest/ProdutoResources.java
@@ -1,7 +1,38 @@
 package br.com.larimaia.rest;
 
+import br.com.larimaia.bo.ProdutoBO;
+import br.com.larimaia.entity.Produto;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
 /**
  * Created by Usuario on 23/09/2015.
  */
+
+@Path("/produto")
 public class ProdutoResources {
+    @Inject
+    ProdutoBO produtoBO;
+
+    @Path("/salvar")
+    @Consumes("application/json")
+    public void salvar(Produto produto){
+      produtoBO.salvar(produto);
+    }
+
+    @GET
+    @Path("/editar/{id}")
+    public Response editar(@PathParam("id") int id){
+        return Response.status(200).entity(produtoBO.editar(id)).build();
+    }
+
+
+    @DELETE
+    @Path("/excluir/{id}")
+    public void excluir(@PathParam("id") int id){
+          produtoBO.remover(id);
+    }
+
 }
