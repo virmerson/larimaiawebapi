6d4f2708063c9923f2fe499ef345b09cd1e30913
davigiroux <daavigiroux@gmail.com>
2015-09-23 20:53:51 -0400

    Criando Entities e criando BO e DAO da entidade Cliente

---
+++ src/main/java/br/com/larimaia/bo/ClienteBO.java
@@ -0,0 +1,39 @@
+package br.com.larimaia.bo;
+
+import br.com.larimaia.dao.ClienteDAO;
+import br.com.larimaia.entity.Cliente;
+
+import javax.enterprise.context.Dependent;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Davi on 23/09/2015.
+ */
+@Dependent
+public class ClienteBO {
+
+    private ClienteDAO cliDao = new ClienteDAO();
+
+
+
+    public ClienteBO(){
+    }
+
+    public void salvar(Cliente cliente){
+        try {
+            cliDao.salvar(cliente);
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public List<Cliente> listar(){
+        return cliDao.buscarTodos();
+    }
+
+    public void remover(Cliente cliente){
+        cliDao.excluir(cliente);
+    }
+}
+
